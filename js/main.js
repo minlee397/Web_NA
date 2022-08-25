b81d54e5b762e959036c904edbc8ead737695ebd
Ly Ba Dong <min.lee397@gmail.com>
2022-08-25 20:16:04 +0700

    dong

--- js/main.js
+++ js/main.js
@@ -22,5 +22,13 @@
 		dots: true // Adds the dots on the bottom
 	});
 
+	$(".slick-carousel2").slick({
+		infinite: true,
+		slidesToShow: 2, // Shows a three slides at a time
+		slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+		arrows: true, 
+		dots: true // Adds the dots on the bottom
+	});
+
 
 })(jQuery);
