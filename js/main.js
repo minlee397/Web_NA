df8590748d4308118c78477f419d1b794b8cac96
Ly Ba Dong <min.lee397@gmail.com>
2022-09-11 19:08:01 +0700

    donglb

--- js/main.js
+++ js/main.js
@@ -18,13 +18,13 @@
 	var widthWindow = window.outerWidth;
 	var slidesToShow = 2;
 	var listCarouselBlog = 3;
-	if (widthWindow < 768){
+	if (widthWindow < 768) {
 		slidesToShow = 1;
 	}
-	if (widthWindow < 769){
+	if (widthWindow < 769) {
 		listCarouselBlog = 2;
 	}
-	if (widthWindow < 768){
+	if (widthWindow < 768) {
 		listCarouselBlog = 1;
 	}
 
@@ -63,7 +63,7 @@
 		//arrows: true,
 		autoplay: true,
 		autoplaySpeed: 2000
-		
+
 	});
 
 	$(".listCarouselCareers").slick({
@@ -86,32 +86,99 @@
 		}
 	});
 
-	$(".boxScrollTop").click(function(){
-		window.scrollTo({top: 0, behavior: 'smooth'});
+	$(".boxScrollTop").click(function () {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		});
 	});
 
-	$(".fileCV").click(function(){
+	$(".fileCV").click(function () {
 		$("#ctrFileCV").click();
 	})
 
-	$("#ctrFileCV").change(function(){
+	$("#ctrFileCV").change(function () {
 		var input = this;
-		if(input.files != undefined && input.files.length > 0)
-		{
+		if (input.files != undefined && input.files.length > 0) {
 			$("#txtCVFile").val(input.files[0].name);
 		}
 	});
-	
-	$( "body" ).click(function() {
+
+	$("body").click(function () {
 		if (!$(event.target).closest('.navbar-nav').length) {
-			$(".menu-level1").css('display','none');
-		  }
-		
+			$(".menu-level1").css('display', 'none');
+		}
+
 	});
-	$( "#giaiphap" ).click(function() {
-		if($(".menu-level1").css('display')=="none")		
-			$(".menu-level1").css('display','block');				
+	$("#giaiphap").click(function () {
+		if ($(".menu-level1").css('display') == "none")
+			$(".menu-level1").css('display', 'block');
 		else
-			$(".menu-level1").css('display','none');
+			$(".menu-level1").css('display', 'none');
 	});
+
+
+	SetPositionImage();
+
 })(jQuery);
+
+function SetPositionImage() {
+	var boxContentImage_1 = $("#boxContentImage_1");
+	var WithContentImage_1 = boxContentImage_1.width();
+	var GroupImage_1 = $("#GroupImage_1");
+	var dataGroupImage_1 = GroupImage_1.find(".divitem");
+
+	var fristLine = true;
+	var TotalWith = 0;
+	var bestWidth = 0;
+
+	var ListHighImage = [];
+
+	if (dataGroupImage_1 != undefined && dataGroupImage_1.length > 0) {
+		for (let index = 0; index < dataGroupImage_1.length; index++) {
+			const element = dataGroupImage_1[index];
+			var boxImage = $(element).find(".boxImage");
+			var heightImage = $(boxImage).find("img").height();
+
+			if (fristLine) {
+				setPosition(boxImage, bestWidth, 0);
+				// Luu lai danh sach chieu cao cua dong dau tien
+				ListHighImage.push({
+					with: bestWidth,
+					height: heightImage
+				});
+				bestWidth += 252;
+				if ((bestWidth + 252) >= WithContentImage_1) {
+					TotalWith = bestWidth;
+					bestWidth = 0;
+					fristLine = false;
+				}
+			} else { // Không phải dòng đầu nữa thì tìm trong danh sách render theo obj nao co chieu cao lon nhat
+				var objMax = ListHighImage.reduce(function (prev, curr) {
+					return prev.height < curr.height ? prev : curr;
+				});
+
+				setPosition(boxImage, bestWidth, objMax.height);
+				bestWidth += 252;
+				objMax.height += heightImage;
+				if ((bestWidth + 252) >= WithContentImage_1) {
+					bestWidth = 0;
+					fristLine = false;
+				}
+			}
+
+
+		}
+
+		// Set lai kich thuoc chieu cao cua boxContent
+		var objMax = ListHighImage.reduce(function (prev, curr) {
+			return prev.height < curr.height ? prev : curr;
+		});
+		boxContentImage_1.css("height", objMax.height +"px");
+		boxContentImage_1.css("width", TotalWith +"px");
+	}
+}
+
+function setPosition(element, x, y) {
+	$(element).css("transform", "translate(" + x + "px, " + y + "px)")
+}
